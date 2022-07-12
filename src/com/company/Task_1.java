82af908c047a50fe497d639d805f2b9a5848b31f
Maksim Shvechkov <shvechkov_maks@mail.ru>
2022-07-12 08:37:22 +0300

    first commit

---
+++ src/com/company/Task_1.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Task_1 {
+
+    public static void main(String[] args) {run();}
+    public static void run(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
